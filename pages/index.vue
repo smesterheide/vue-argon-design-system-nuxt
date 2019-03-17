1368213da58724d03a688b3e525fa1f1c919e1b3
sames <1752390+sambrezo@users.noreply.github.com>
2019-03-17 11:53:06 +0100

    Merge Nuxt index page into Argon starter template

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
-        <hero>Hello World</hero>
+        <hero>
+            <logo/><br/>
+            Hello World
+        </hero>
     </div>
 </template>
 <script>
 import Hero from "~/components/argon-demo/Hero";
+import Logo from "~/components/Logo.vue";
 export default {
   components: {
-      Hero
+      Hero,
+      Logo
   }
 };
 </script>
